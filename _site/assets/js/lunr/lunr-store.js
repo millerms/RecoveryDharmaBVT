bb1ca67be3f01c617fdaa0a3f0268d6d843bc54b
Michael Miller <michaelmiller@Mac.home.local>
2025-08-02 12:59:27 -0400

    added gemfile for local builds

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,31 @@
+var store = [{
+        "title": "Buddhist Foundations",
+        "excerpt":"Buddhist Foundations Welcome! This page introduces Buddhist philosophy and ethics in a non-dogmatic, beginner-friendly way, especially for people in recovery. “Craving is the root of suffering. If you remove craving, you remove suffering.” — The Buddha On This Page Buddhist Foundations On This Page What Is Buddhism? Ethics &amp; Practice...","categories": [],
+        "tags": [],
+        "url": "/RecoveryDharmaBVT/buddhist-foundations/",
+        "teaser": null
+      },{
+        "title": "Contact Us",
+        "excerpt":"We’d love to hear from you! Whether you have questions, want to share an event, or just want to connect, please reach out to (this is a dummy email, will replace soon.) - info@recoverydharmavt.org.    ","categories": [],
+        "tags": [],
+        "url": "/RecoveryDharmaBVT/contact/",
+        "teaser": null
+      },{
+        "title": "Events",
+        "excerpt":"Coming Soon.   This is a placeholder event. Date is tentative and subject to change. Check back for updates.     October, 10-12 2025 – Mindful Recovery Retreat, Adamant, VT    ","categories": [],
+        "tags": [],
+        "url": "/RecoveryDharmaBVT/events/",
+        "teaser": null
+      },{
+        "title": "Burlington Meetings & Groups",
+        "excerpt":"Recovery Dharma Meetings and Meditation Groups in Burlington, VT Recovery Dharma - Burlington Wednesday Night Date &amp; Time: Wednesday @ 6pm - 7pm Type: In-person Address: 21 Buell St, Burlington, VT 05401 Recovery Dharma - Burlington Thursday Afternoon Date &amp; Time: Thursday @ 1pm - 2pm Type: In-person Address: 179...","categories": [],
+        "tags": [],
+        "url": "/RecoveryDharmaBVT/meetings/",
+        "teaser": null
+      },{
+        "title": "Resources",
+        "excerpt":"Resources for Recovery and Meditation RD Meetings Recovery Dharma Vermont Meetings Recovery Dharma Online Meetings RD Book and Meditations Recovery Dharma Book (PDF) Guided Recovery Dharma Meditations RD Online Resources Recovery Dharma Website Recovery Dharma YouTube Channel Recovery Dharma Facebook Group Other Meditation Groups in Burlington Burlington Dharma Collective New...","categories": [],
+        "tags": [],
+        "url": "/RecoveryDharmaBVT/resources/",
+        "teaser": null
+      },]
